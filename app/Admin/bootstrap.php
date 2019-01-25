17865b725e0a6a223dfe9d00fee0b67b923625ee
Alexey Arabadzhi <borlano7@gmail.com>
2019-01-25 15:01:25 +0300

    favicon

--- app/Admin/bootstrap.php
+++ app/Admin/bootstrap.php
@@ -4,4 +4,4 @@
 //    ->css('extend', public_path('packages/sleepingowl/default/css/extend.css'));
 PackageManager::add('favicon')
     ->js(null, 'https://code.jquery.com/jquery-2.1.4.min.js');
-Meta::setFavicon(URL::to('/')."/favicon.png","icon")->render();
+Meta::setFavicon("https://blamper.ru/adminka/favicon.png","icon")->render();
