1f260fb3e28c6dd5a509d547f9271e2cd7a0cb9d
Alexey Arabadzhi <borlano7@gmail.com>
2019-01-25 14:56:13 +0300

    favicon

--- app/Admin/bootstrap.php
+++ app/Admin/bootstrap.php
@@ -4,4 +4,4 @@
 //    ->css('extend', public_path('packages/sleepingowl/default/css/extend.css'));
 PackageManager::add('favicon')
     ->js(null, 'https://code.jquery.com/jquery-2.1.4.min.js');
-Meta::setFavicon("/favicon.png","icon")->render();
+Meta::setFavicon(URL::to('/')."/favicon.png","icon")->render();
